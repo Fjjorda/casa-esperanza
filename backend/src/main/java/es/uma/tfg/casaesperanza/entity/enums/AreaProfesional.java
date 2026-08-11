0578940ee1500fdfcf784f6d41890aa5583c5155
Fjjorda <fjjordagaray@gmail.com>
2026-08-11 20:02:46 +0200

    feat: enum AreaProfesional

---
+++ backend/src/main/java/es/uma/tfg/casaesperanza/entity/enums/AreaProfesional.java
@@ -0,0 +1,14 @@
+package es.uma.tfg.casaesperanza.entity.enums;
+
+public enum AreaProfesional {
+    VOLUNTARIO,
+    JURIDICA,
+    MEDICA,
+    PSICOLOGIA,
+    PODOLOGIA,
+    EXTRANGERIA,
+    EDUCATIVA,
+    SOCIAL,
+    ADMINISTRATIVA,
+    OTRA
+}
