dffb70d959b68c0903189696e81a3f13d94b89cd
Fjjorda <fjjordagaray@gmail.com>
2026-08-17 23:59:41 +0200

    feat: AreaProfesionalRepository

---
+++ backend/src/main/java/es/uma/tfg/casaesperanza/repository/AreaProfesionalRepository.java
@@ -0,0 +1,7 @@
+package es.uma.tfg.casaesperanza.repository;
+
+import es.uma.tfg.casaesperanza.entity.AreaProfesionalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaProfesionalRepository extends JpaRepository<AreaProfesionalEntity, Integer> {
+}
