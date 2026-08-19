ecabc419878fac474508f7871ae32944cac2e882
Fjjorda <fjjordagaray@gmail.com>
2026-08-20 01:16:15 +0200

    refactor: enum Rol
    Se separan dos Sets inmutables para cada Rol: capacidades inherentes y capacidades opcionales con sus apropiados getters.

--- backend/src/main/java/es/uma/tfg/casaesperanza/entity/enums/Rol.java
+++ backend/src/main/java/es/uma/tfg/casaesperanza/entity/enums/Rol.java
@@ -1,5 +1,8 @@
 package es.uma.tfg.casaesperanza.entity.enums;
 
+import lombok.Getter;
+
+import java.util.EnumSet;
 import java.util.Set;
 
 /**
@@ -10,65 +13,120 @@ import java.util.Set;
  */
 public enum Rol {
     ADMIN(
-            Capacidad.AGENDAR_SERVICIOS,
-            Capacidad.CONCEDER_HABILITACION_TEMPORAL,
-            Capacidad.CONSULTAR_LOG_AUDITORIA,
-            Capacidad.CONSULTAR_NOTAS_AMIGO,
-            Capacidad.CREAR_ASIENTOS,
-            Capacidad.CREAR_NOTA_PRIVADA,
-            Capacidad.ENTREGAR_ROPA,
-            Capacidad.ESCALAR_NIVEL_3,
-            Capacidad.EXPORTAR_INFORMES,
-            Capacidad.GESTIONAR_ARMARIO,
-            Capacidad.GESTIONAR_CUENTAS_USUARIO,
-            Capacidad.IMPORTAR_DATOS,
-            Capacidad.IMPORTAR_EXTRACTOS,
-            Capacidad.OPERAR_LAVANDERIA,
-            Capacidad.REGISTRAR_ENTREGA_ALIMENTARIA,
-            Capacidad.REGISTRAR_GASTO_AMIGO,
-            Capacidad.REGISTRAR_SALIDA_ALIMENTARIA,
-            Capacidad.REGISTRAR_AMIGO
+            // Capacidades inherentes
+            Set.of(
+                    Capacidad.AGENDAR_SERVICIOS,
+                    Capacidad.CONCEDER_HABILITACION_TEMPORAL,
+                    Capacidad.CONSULTAR_LOG_AUDITORIA,
+                    Capacidad.CONSULTAR_NOTAS_AMIGO,
+                    Capacidad.CREAR_ASIENTOS,
+                    Capacidad.CREAR_NOTA_PRIVADA,
+                    Capacidad.ENTREGAR_ROPA,
+                    Capacidad.ESCALAR_NIVEL_3,
+                    Capacidad.EXPORTAR_INFORMES,
+                    Capacidad.GESTIONAR_ARMARIO,
+                    Capacidad.GESTIONAR_CUENTAS_USUARIO,
+                    Capacidad.IMPORTAR_DATOS,
+                    Capacidad.IMPORTAR_EXTRACTOS,
+                    Capacidad.OPERAR_LAVANDERIA,
+                    Capacidad.REGISTRAR_ENTREGA_ALIMENTARIA,
+                    Capacidad.REGISTRAR_GASTO_AMIGO,
+                    Capacidad.REGISTRAR_SALIDA_ALIMENTARIA,
+                    Capacidad.REGISTRAR_AMIGO),
+
+            // Capacidades opcionales
+            Set.of()
     ),
     OPERATIVO(
-            Capacidad.AGENDAR_SERVICIOS,
-            Capacidad.CREAR_NOTA_PRIVADA,
-            Capacidad.ENTREGAR_ROPA,
-            Capacidad.GESTIONAR_ARMARIO,
-            Capacidad.IMPORTAR_DATOS,
-            Capacidad.OPERAR_LAVANDERIA,
-            Capacidad.REGISTRAR_ENTREGA_ALIMENTARIA,
-            Capacidad.REGISTRAR_SALIDA_ALIMENTARIA,
-            Capacidad.REGISTRAR_AMIGO
+            // Capacidades inherentes
+            Set.of(
+                    Capacidad.AGENDAR_SERVICIOS,
+                    Capacidad.CREAR_NOTA_PRIVADA,
+                    Capacidad.ENTREGAR_ROPA,
+                    Capacidad.GESTIONAR_ARMARIO,
+                    Capacidad.IMPORTAR_DATOS,
+                    Capacidad.OPERAR_LAVANDERIA,
+                    Capacidad.REGISTRAR_ENTREGA_ALIMENTARIA,
+                    Capacidad.REGISTRAR_SALIDA_ALIMENTARIA,
+                    Capacidad.REGISTRAR_AMIGO
+            ),
+
+            // Capacidades opcionales
+            Set.of(
+                    Capacidad.CONSULTAR_NOTAS_AMIGO,
+                    Capacidad.CREAR_ASIENTOS,
+                    Capacidad.ESCALAR_NIVEL_3,
+                    Capacidad.EXPORTAR_INFORMES,
+                    Capacidad.REGISTRAR_GASTO_AMIGO
+            )
 
     ),
     PROFESIONAL(
-            Capacidad.AGENDAR_SERVICIOS,
-            Capacidad.CREAR_NOTA_PRIVADA,
-            Capacidad.GESTIONAR_ARMARIO
+            // Capacidades inherentes
+            Set.of(
+                    Capacidad.AGENDAR_SERVICIOS,
+                    Capacidad.CREAR_NOTA_PRIVADA,
+                    Capacidad.GESTIONAR_ARMARIO
+            ),
+
+            // Capacidades opcionales
+            Set.of(Capacidad.CONSULTAR_NOTAS_AMIGO)
     ),
     PUENTE(
-            Capacidad.CREAR_NOTA_PRIVADA,
-            Capacidad.GESTIONAR_ARMARIO
+            Set.of(
+                    Capacidad.CREAR_NOTA_PRIVADA,
+                    Capacidad.GESTIONAR_ARMARIO
+            ),
+
+            // Capacidades opcionales
+            Set.of(
+                    Capacidad.AGENDAR_SERVICIOS,
+                    Capacidad.OPERAR_LAVANDERIA
+            )
     ),
     AGENTE_TEMP(
-            Capacidad.CREAR_NOTA_PRIVADA
+            // Capacidades inherentes
+            Set.of(Capacidad.CREAR_NOTA_PRIVADA),
+
+            // Capacidades opcionales
+            Set.of(
+                    Capacidad.CONSULTAR_NOTAS_AMIGO,
+                    Capacidad.EXPORTAR_INFORMES,
+                    Capacidad.GESTIONAR_ARMARIO
+            )
+
     );
 
-    // Creamos el Set que almacena las capacidades inherentes de cada Rol como final para que esa referencia
+    // Creamos los Sets que almacenan las capacidades inherentes y opcionales de cada Rol como final para que esa referencia
     // no pueda apuntar a otro Set después de construirse la instancia
+    @Getter
     private final Set<Capacidad> capacidadesInherentes;
+    @Getter
+    private final Set<Capacidad> capacidadesOpcionales;
 
-    // Constructor con argumentos variables que crea cada instancia de Rol en tiempo de ejecución
-    // Cada instancia tiene su propio Set inmutable y final de capacidades inherentes
-    Rol(Capacidad... capacidades) {
-        this.capacidadesInherentes = Set.of(capacidades);
+    // Constructor que instancia cada Rol con sus capacidades. Cada conjunto es inmutable
+    Rol(Set<Capacidad> capacidadesInherentes, Set<Capacidad> capacidadesOpcionales) {
+        this.capacidadesInherentes = capacidadesInherentes;
+        this.capacidadesOpcionales = capacidadesOpcionales;
     }
 
-    public Set<Capacidad> getCapacidadesInherentes() {
-        return capacidadesInherentes;
+    /**
+     * Calcula las capacidades que NO están disponibles para este rol.
+     * <p>
+     * (Todas las Capacidades - Inherentes - Opcionales)
+     */
+    public Set<Capacidad> getCapacidadesIncompatibles() {
+        EnumSet<Capacidad> capacidadesIncompatibles = EnumSet.allOf(Capacidad.class);
+        capacidadesIncompatibles.removeAll(this.capacidadesInherentes);
+        capacidadesIncompatibles.removeAll(this.capacidadesOpcionales);
+
+        return capacidadesIncompatibles;
     }
 
-    public boolean tiene(Capacidad capacidad) {
-        return capacidadesInherentes.contains(capacidad);
+    /**
+     * Consulta si una capacidad se encuentra en el conjunto de capacidades opcionales.
+     */
+    public boolean puedeTener(Capacidad capacidad) {
+        return capacidadesOpcionales.contains(capacidad);
     }
 }
