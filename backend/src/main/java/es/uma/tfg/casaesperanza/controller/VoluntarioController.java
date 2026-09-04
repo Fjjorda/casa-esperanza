a45d108f6c5dbddd2c44ed5062de06e0530930c9
Fjjorda <fjjordagaray@gmail.com>
2026-09-04 23:53:48 +0200

    feat: endpoint POST en VoluntarioController
    Se crea un endpoint tipo POST con @Valid @RequestBody CreateVoluntarioRequest para activar Bean Validation antes de ejecutar el método con un try/catch inline con el mensaje en el body ({"Error!": "..."}) en vez del 500 genérico por defecto de Spring cuando ocurre un error de validación.

--- backend/src/main/java/es/uma/tfg/casaesperanza/controller/VoluntarioController.java
+++ backend/src/main/java/es/uma/tfg/casaesperanza/controller/VoluntarioController.java
@@ -1,13 +1,15 @@
 package es.uma.tfg.casaesperanza.controller;
 
+import es.uma.tfg.casaesperanza.dto.request.CreateVoluntarioRequest;
 import es.uma.tfg.casaesperanza.dto.response.VoluntarioListResponse;
 import es.uma.tfg.casaesperanza.service.VoluntarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/casa-esperanza/voluntarios")
@@ -22,5 +24,15 @@ public class VoluntarioController {
     @GetMapping
     public ResponseEntity<List<VoluntarioListResponse>> listarVoluntarios() {
         return ResponseEntity.ok(voluntarioService.listarVoluntarios());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearVoluntario(@Valid @RequestBody CreateVoluntarioRequest request) {
+        try {
+            voluntarioService.crearCuentaVoluntario(request);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.badRequest().body(Map.of("Error!", exception.getMessage()));
+        }
     }
 }
